f81c1de5ffc0b477a39156fdf73c127098d205c0
IbrahimAhmed <ibrahimahmed221993@gmail.com>
2023-06-07 04:51:05 +0300

    fix response content type

--- api/middlewares/async-handler.ts
+++ api/middlewares/async-handler.ts
@@ -3,11 +3,14 @@ import { validationResult } from "express-validator";
 
 const asyncHandler: (handler: Handler) => Handler = (handler) => {
     return async (req, res, next) => {
+        res.setHeader('Content-Type', 'application/json');
+
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
         try {
+
             await handler(req, res, next);
         } catch (error) {
             console.error(error);
--- api/middlewares/content-type.ts
+++
@@ -1,4 +0,0 @@
-const setContentType = (req, res, next) => {
-    res.setHeader('Content-Type', 'application/json');
-    next();
-}
