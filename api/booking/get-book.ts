db18dcb7b477b5ba755fe7c2b9570b7e365593c6
IbrahimAhmed <ibrahimahmed221993@gmail.com>
2023-06-11 02:38:57 +0300

    fix get book api

--- api/booking/get-book.ts
+++ api/booking/get-book.ts
@@ -6,8 +6,14 @@ import asyncHandler from "../middlewares/async-handler";
 const getbook = asyncHandler(async (req, res) => {
     const userID = req.user?._id.toString()
     const { bookingStatus } = req.query
-    var bookes = await Booking.find({ userID ,bookingStatus})
-    res.json(bookes)
+    if (bookingStatus) {
+        var bookes = await Booking.find({ userID, bookingStatus })
+        res.json(bookes)
+    } else {
+        var bookes = await Booking.find({ userID })
+        res.json(bookes);
+    }
+
 })
 
 export default getbook;
