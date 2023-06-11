1cb2a7610b7a2bc1ef2fcd6d326d22a39942352e
IbrahimAhmed <ibrahimahmed221993@gmail.com>
2023-06-11 07:13:06 +0300

    fix cronJob

--- cronJob/cronJob.ts
+++ cronJob/cronJob.ts
@@ -5,7 +5,7 @@ import { connectionPromise } from '../db';
 connectionPromise.then(() => {
     cron.schedule('*/1 * * * *', async () => {
         try {
-            var rows = await Booking.find().populate('periodID').exec();
+            var rows = await Booking.find();
             for (const row of rows) {
 
                 await processRow(row);
@@ -21,9 +21,15 @@ connectionPromise.then(() => {
 
 
 const processRow = async (row) => {
-    var currentTime = new Date();
-    var bookingEndTime = row.periodID?.endTime ?? new Date()
-    if (row.bookingStatus == "Active" && currentTime > bookingEndTime) {
+    const now: Date = new Date();
+    var latestTime: Date = row.periodIDs.reduce((latest, current) => {
+        if (!latest || current.endTime > latest.endTime) {
+            return current;
+        }
+        return latest;
+    }, undefined)?.endTime;
+    if (row.bookingStatus == "Active" && now > latestTime) {
+        console.log("updated Row")
         await row.updateOne({ bookingStatus: "OutDated" }, { new: true })
     }
 
