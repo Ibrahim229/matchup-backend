e920d549c0d1a13ddf8635fda06551f9178ded09
IbrahimAhmed <ibrahimahmed221993@gmail.com>
2023-07-06 00:03:58 +0300

    cors enable test 2

--- index.ts
+++ index.ts
@@ -14,7 +14,11 @@ config();
 const app = express();
 
 var bodyParser = require('body-parser')
-app.use(cors)
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
 app.use(bodyParser.json()); // Parse JSON bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
