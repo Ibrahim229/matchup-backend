8d1350e7a9df97f27ba9140ca4525b410742b50a
IbrahimAhmed <ibrahimahmed221993@gmail.com>
2023-07-09 04:13:57 +0300

    change http method

--- api/profile/index.ts
+++ api/profile/index.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import upload from "../../config/multer";
 import updateUserPic from "./update-user-pic";
+import changeFullName from "./change-full-name";
 
 
 
 const profileRouter = Router();
 profileRouter.put("/updateProfilePic", upload.single("image"), updateUserPic)
-profileRouter.get("/changeFullName/:fullName", updateUserPic)
+profileRouter.get("/changeFullName/:fullName", changeFullName)
 
 
 
