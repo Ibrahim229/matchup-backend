4ac0ead3685ecec627084a1695192945329bac79
IbrahimAhmed <ibrahimahmed221993@gmail.com>
2023-07-11 01:56:41 +0300

    change verify user res

--- api/auth/verify-token.ts
+++ api/auth/verify-token.ts
@@ -3,7 +3,9 @@ import asyncHandler from '../middlewares/async-handler';
 
 const verifyToken = asyncHandler(async (req, res) => {
     if (req.user?.role == "User") {
-        res.json(true)
+        var userID = req.user._id;
+        var userData = req.user?.toJson();
+        res.json({ valid: true, userData: userData, userID: userID })
     } else {
         res.status(401).json({ error: "Unauthorized" })
     }
