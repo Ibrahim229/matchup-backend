f3cbb19c4fdc8d6f496e54729f6b3755e3fee8cc
IbrahimAhmed <ibrahimahmed221993@gmail.com>
2023-07-17 20:15:21 +0300

    fix time

--- api/pitch/update-close-time.ts
+++ api/pitch/update-close-time.ts
@@ -5,25 +5,27 @@ import asyncHandler from "../middlewares/async-handler";
 const updateCloseTime = asyncHandler(async (req, res) => {
     if (req.user?.role == "Admin") {
         const { id } = req.params;
-        const { openT, closeT } = req.body
+        const { openT , closeT } = req.body
+        var open=new Date(openT)
+        var close = new Date(closeT)
         var now = new Date();
         var openTime = new Date(
             now.getFullYear(),
             now.getMonth(),
             now.getDate(),
-            openT.getHours(),
-            openT.getMinutes(),
-            openT.getSeconds(),
-            openT.getMilliseconds()
+            open.getHours(),
+            open.getMinutes(),
+            open.getSeconds(),
+            open.getMilliseconds()
         );
         var closeTime = new Date(
             now.getFullYear(),
             now.getMonth(),
             now.getDate(),
-            closeT.getHours(),
-            closeT.getMinutes(),
-            closeT.getSeconds(),
-            closeT.getMilliseconds()
+            close.getHours(),
+            close.getMinutes(),
+            close.getSeconds(),
+            close.getMilliseconds()
         );
         const pitch = await Pitch.findByIdAndUpdate(id, { openTime, closeTime }, { new: true })
 
