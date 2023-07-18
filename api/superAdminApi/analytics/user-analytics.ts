dca4b57ee3aa54968bd4bfe50a8a039cff12dcbd
IbrahimAhmed <ibrahimahmed221993@gmail.com>
2023-07-18 17:30:08 +0300

    change response object

--- api/superAdminApi/analytics/user-analytics.ts
+++ api/superAdminApi/analytics/user-analytics.ts
@@ -1,5 +1,5 @@
 import asyncHandler from "../../middlewares/async-handler"
-import User from "../../../db/user";
+import User, { UserType } from "../../../db/user";
 import Event from "../../../db/event";
 import { UserAnalytics } from "../../../interfaces/user_methods_interface";
 
@@ -7,13 +7,21 @@ import { UserAnalytics } from "../../../interfaces/user_methods_interface";
 const getUserAnalytics = asyncHandler(async (req, res) => {
     if (req.user?.role == "superAdmin") {
 
-        var users: UserAnalytics[] = await User.find({ role: "User" })
+        var users: UserType[] = await User.find({ role: "User" })
 
-        var usersWithBookings: UserAnalytics[] = await Promise.all(users.map(async (user) => {
+        var usersWithBookings = await Promise.all(users.map(async (user) => {
             var bookings = await Event.find({ user: user._id, status: "OutDated" })
-            user.bookings = bookings.length
-            return user
+            let newUser = {
+                fullName: user.fullName,
+                email: user.email,
+                phoneNumber: user.phoneNumber,
+                gender: user.gender,
+                age: user.age,
+                bookings: bookings.length
+            }
+            return newUser
         }))
+
 
         res.json(usersWithBookings)
     } else {
