89a8f3466cab697619cb870d7029b39799da1f69
IbrahimAhmed <ibrahimahmed221993@gmail.com>
2023-08-02 16:43:52 +0300

    fix delete recommendedapi

--- api/pitch/recommended/get-recommend.ts
+++ api/pitch/recommended/get-recommend.ts
@@ -4,8 +4,9 @@ import asyncHandler from "../../middlewares/async-handler";
 
 const getRecommend = asyncHandler(async (req, res) => {
     var recomended: recommendedType[] = await Recommended.find();
+    console.log("without filter recommended", recomended)
     var recommendedWithoutNull = recomended.filter((element) => element.pitch != null)
-
+    console.log("with filter recommended", recomended)
     res.json(recommendedWithoutNull)
 })
 
