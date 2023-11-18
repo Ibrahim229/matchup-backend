848ef0404ac0e2541df0a9277b2772b6cacd55c4
Mahmoud Kamal <ma7moud.kamal222@email.com>
2023-11-18 15:49:04 +0200

    Feat: Remove Count API

--- api/superAdminApi/index.ts
+++ api/superAdminApi/index.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { addCount } from './add-count';
 import analyticsRoute from './analytics';
 import getSystemPitch from './get-pitch';
 
@@ -7,6 +6,5 @@ const superAdminRouter = Router();
 
 superAdminRouter.get('/pitch', getSystemPitch);
 superAdminRouter.use('/analytics', analyticsRoute);
-superAdminRouter.post('/addCount', addCount);
 
 export default superAdminRouter;
